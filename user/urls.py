5c5f498163ff254d250ef5d7ec830f37c1d271e0
rompu <rom.puh@gmail.com>
2024-09-06 13:43:20 +0300

    added endpoint logout for user

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
+)
 
 from user.views import CreateUserView, ManageUserView
 
@@ -11,5 +16,6 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
     path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
