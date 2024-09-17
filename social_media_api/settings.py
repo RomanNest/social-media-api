836a48c8b585150871fd3695ee9f2c0a3f70394c
rompu <rom.puh@gmail.com>
2024-09-17 12:50:03 +0300

    created permissions.py & implemented it settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "social_api.permissions.IsOwnerOrAdminOrIfAuthenticatedReadOnly",
+    ]
 }
 
 SIMPLE_JWT = {
